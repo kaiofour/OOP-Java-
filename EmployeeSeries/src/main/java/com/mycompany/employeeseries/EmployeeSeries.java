8e262602c14f52936477b80298ef01d7ddeaa250
kaiofour <edullanteskurt@gmail.com>
2024-09-23 01:30:54 +0800

    test git cmd

---
+++ EmployeeSeries/src/main/java/com/mycompany/employeeseries/EmployeeSeries.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.employeeseries;
+
+import version1.HourlyEmployee;
+/**
+ *
+ * @author Russel
+ */
+public class EmployeeSeries {
+
+    public static void main(String[] args) {
+        System.out.println()
+    }
+}
